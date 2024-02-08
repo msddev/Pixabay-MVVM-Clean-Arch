32397bd39f2b05520d8e77359edf2cc5cbbfc491
Masoud Khoshkam <masoud.khoshkamkhatibgourabi@snapp.cab>
2024-02-08 12:48:36 +0330

    Implement domain use case for search unit

--- domain/src/main/java/com/example/domain/usecase/SearchImageUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/SearchImageUseCase.kt
@@ -1,4 +1,14 @@
 package com.example.domain.usecase
 
-class SearchImageUseCase {
+import androidx.paging.PagingData
+import com.example.domain.model.ImageModel
+import com.example.domain.repository.SearchImageRepository
+import kotlinx.coroutines.flow.Flow
+
+class SearchImageUseCase(
+    private val searchImagesRepository: SearchImageRepository
+) {
+    operator fun invoke(payload: String): Flow<PagingData<ImageModel>> {
+        return searchImagesRepository.searchImage(payload)
+    }
 }
